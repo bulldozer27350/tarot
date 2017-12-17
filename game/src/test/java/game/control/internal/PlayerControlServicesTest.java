5df070894a58a30de301e9f461b044e73cbb2a3e
Marco <bulldozer27350@gmail.com>
2017-12-17 18:14:15 +0100

    Adding test cases

--- game/src/test/java/game/control/internal/PlayerControlServicesTest.java
+++ game/src/test/java/game/control/internal/PlayerControlServicesTest.java
@@ -1,9 +1,15 @@
 package game.control.internal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import game.domain.Card;
+import game.domain.Card.Color;
+import game.domain.Done;
 import game.domain.Fold;
 import game.domain.Player;
 
@@ -44,5 +50,85 @@ public class PlayerControlServicesTest {
 		Assert.assertEquals(2, player.getFolds().get(0).getCards().size());
 		Assert.assertTrue(player.getFolds().get(0).getCards().contains(card1));
 		Assert.assertTrue(player.getFolds().get(0).getCards().contains(card2));
+	}
+	
+	@Test
+	public void testGetNotYetOpenedColors(){
+		// Given
+		Done emptyDone = new Done();
+		Done done = new Done();
+		final Fold foldCoeur = new Fold();
+		foldCoeur.setPlayedColor(Color.HEART);
+		Player player = new Player();
+		player.getFolds().add(foldCoeur);
+		done.getPlayers().add(player);
+		PlayerControlServicesImpl playerControlServices = new PlayerControlServicesImpl(null);
+		
+		// When
+		List<Color> notYetOpenedColors = playerControlServices.getNotYetOpenedColors(emptyDone);
+		
+		// Then
+		Assert.assertEquals(Arrays.asList(Color.SPADE, Color.HEART, Color.DIAMOND, Color.CLUB),  notYetOpenedColors);
+		
+		// When
+		notYetOpenedColors = playerControlServices.getNotYetOpenedColors(done);
+		
+		// Then
+		Assert.assertEquals(Arrays.asList(Color.SPADE, Color.DIAMOND, Color.CLUB),  notYetOpenedColors);
+	}
+	
+	@Test
+	public void testGetAlreadyOpenedColors(){
+		// Given
+		Done emptyDone = new Done();
+		Done done = new Done();
+		final Fold foldCoeur = new Fold();
+		foldCoeur.setPlayedColor(Color.HEART);
+		Player player = new Player();
+		player.getFolds().add(foldCoeur);
+		done.getPlayers().add(player);
+		PlayerControlServicesImpl playerControlServices = new PlayerControlServicesImpl(null);
+		
+		// When
+		List<Color> notYetOpenedColors = playerControlServices.getAlreadyOpenedColors(emptyDone);
+		
+		// Then
+		Assert.assertEquals(Arrays.asList(),  notYetOpenedColors);
+		
+		// When
+		notYetOpenedColors = playerControlServices.getAlreadyOpenedColors(done);
+		
+		// Then
+		Assert.assertEquals(Arrays.asList(Color.HEART),  notYetOpenedColors);
+	}
+	
+	@Test
+	public void testSelectRandomTrump(){
+		// Given
+		Done done = new Done();
+		Card randomCard= new Card();
+		List<Card> cards = new ArrayList<>();
+		List<Fold> previousFolds = new ArrayList<>();
+		final Fold fold = new Fold();
+		Player player = new Player();
+		PlayerControlServicesImpl playerControlServices = new PlayerControlServicesImpl(null){
+			@Override
+			Card selectColorCardToPlay(Done done, Player player, List<Card> cards, Fold fold,
+					List<Fold> previousFolds) {
+				return randomCard;
+			}
+		};
+		
+		// When
+		Card card = playerControlServices.selectRandomTrump(done, player, cards, fold, previousFolds);
+		
+		// Then
+		Assert.assertEquals(randomCard, card);
+	}
+	
+	@Test
+	public void testSelectColorCardToPlay() {
+		// should be implemented because it has been overiden in another test
+		Assert.fail();
 	}
 }
